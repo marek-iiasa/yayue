dfdf87901855752c7b9d9fcafb6ff6c8607d66d5
Marek Makowski <marek@iiasa.ac.at>
2023-06-18 13:20:40 +0200

    mcma: removed redundant comments
    Meanings of the model entities are now specified ad the doc option of the corresponding objects.

--- models/mcma/t4conc.py
+++ models/mcma/t4conc.py
@@ -25,21 +25,21 @@ def mk_conc():
     m.goal.activate()
 
     # parameters  (declared in the sequence corresponding to their use in SMS)
-    m.ub1 = pe.Param(domain=pe.NonNegativeReals, default=10., doc='up-bnd for total activity')   # RHS for con1
-    m.lb1 = pe.Param(domain=pe.NonNegativeReals, default=2., doc='lo-bnd for total activity')   # LHS for con1
-    m.px = pe.Param(domain=pe.NonNegativeReals, default=10., doc='price of x')   # price of x
-    m.py = pe.Param(domain=pe.NonNegativeReals, default=0.5, doc='price of y')   # price of y
-    m.pz = pe.Param(domain=pe.NonNegativeReals, default=0.1, doc='price of z')   # price of z
-    m.ex = pe.Param(domain=pe.NonNegativeReals, default=0.1, doc='unit emission of x')   # emission of x
-    m.ey = pe.Param(domain=pe.NonNegativeReals, default=0.5, doc='unit emission of y')   # emission of y
-    m.ez = pe.Param(domain=pe.NonNegativeReals, default=10., doc='unit emission of z')   # emission of z
+    m.ub1 = pe.Param(domain=pe.NonNegativeReals, default=10., doc='up-bnd for total activity')
+    m.lb1 = pe.Param(domain=pe.NonNegativeReals, default=2., doc='lo-bnd for total activity')
+    m.px = pe.Param(domain=pe.NonNegativeReals, default=10., doc='price of x')
+    m.py = pe.Param(domain=pe.NonNegativeReals, default=0.5, doc='price of y')
+    m.pz = pe.Param(domain=pe.NonNegativeReals, default=0.1, doc='price of z')
+    m.ex = pe.Param(domain=pe.NonNegativeReals, default=0.1, doc='unit emission of x')
+    m.ey = pe.Param(domain=pe.NonNegativeReals, default=0.5, doc='unit emission of y')
+    m.ez = pe.Param(domain=pe.NonNegativeReals, default=10., doc='unit emission of z')
 
     # relations (constraints)
     # m.con1 = pe.Constraint(expr=(m.b2 <= m.x + m.y + m.z <= m.b1))  # range split into two because results in error
-    m.con1 = pe.Constraint(expr=(m.x + m.y + m.z <= m.ub1), doc='up-bnd for production level')  # production range upp-bnd
-    m.con2 = pe.Constraint(expr=(m.lb1 <= m.x + m.y + m.z), doc='up-bnd for production level')  # production range low-bnd
-    m.incD = pe.Constraint(expr=(m.inc == m.px * m.x + m.py * m.y + m.pz * m.z), doc='specs of income')  # income
-    m.emiD = pe.Constraint(expr=(m.emi == m.ex * m.x + m.ey * m.y + m.ez * m.z), doc='specs of emission')  # emission
+    m.con1 = pe.Constraint(expr=(m.x + m.y + m.z <= m.ub1), doc='up-bnd for production level')
+    m.con2 = pe.Constraint(expr=(m.lb1 <= m.x + m.y + m.z), doc='up-bnd for production level')
+    m.incD = pe.Constraint(expr=(m.inc == m.px * m.x + m.py * m.y + m.pz * m.z), doc='specs of income')
+    m.emiD = pe.Constraint(expr=(m.emi == m.ex * m.x + m.ey * m.y + m.ez * m.z), doc='specs of emission')
 
     # m.pprint()
     print(f'mk_conc(): concrete model "{m.name}" generated.')
