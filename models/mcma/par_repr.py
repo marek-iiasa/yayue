be732b5e500a99dfe39144462144d5acebabd513
Marek Makowski <marek@iiasa.ac.at>
2023-08-28 23:30:08 +0200

    mcma: ParRep slightly advanced.
    Minor additions and corrections.

--- models/mcma/par_repr.py
+++ models/mcma/par_repr.py
@@ -11,9 +11,10 @@ class ParSol:     # one Pareto solution
 
 
 class Cube:     # one Pareto solution
-    def __init__(self, s1, s2):
+    def __init__(self, s1, s2, size):
         self.s1 = s1    # id of first solution defining the cube
         self.s2 = s2    # id of second solution defining the cube
+        self.size = size    # cube size (L1 distance(s1, s2)
         self.asp_asf = []   # list of A values in the ASF scale
         self.res_asf = []   # list of R values in the ASF scale
         self.asp_vals = []   # list of A values in the core scale
@@ -23,14 +24,14 @@ class Cube:     # one Pareto solution
 class ParRep:     # representation of Pareto set
     def __init__(self, mc):
         self.mc = mc    # CtrMca object
-        self.n_crit = mc.n_crit   # number of criteria
+        # self.n_crit = mc.n_crit   # number of criteria  (not needed?)
         self.n_corners = 0   # number of corners (one utopia + other inactive)
         self.sols = []  # Pareto-solutions (ParSol objects)
         self.neigh = []  # list of pairs [seq_id and distance to] defining closest neighbor
         self.cubes = []     # list of cubes (Cube objects, one cube for each iteration)
 
         self.ini_corners()  # initialize corner solutions
-        self.nearest()
+        self.nearest()      # find pairs of neighbors (nearest solutions)
         # raise Exception(f'ParRep ctor not implemented yet.')
 
     def ini_corners(self):  # initialize corner solutions (each composed of one utopia and nadir of all others)
@@ -74,7 +75,7 @@ class ParRep:     # representation of Pareto set
         pass
 
     def cube(self):  # define new cube (in ASF space), set A/R (in both ASF and A/R scales)
-        # find pairs of neighbor solutions defining largest cube
+        # find pairs of neighbor solutions defining the largest cube
         mx_dis = 0.    # initialize distance
         s1 = None   # initialize solutions to undefined
         s2 = None
@@ -85,6 +86,10 @@ class ParRep:     # representation of Pareto set
                 s1 = i
                 s2 = n2
                 mx_dis = dis
-        assert mx_dis > 0., f'ParRep::cube(): cube is undefined.'
-        print(f'Cube is defined by solutions ({s1}, {s2}), cube size = {mx_dis}.')
-        raise Exception(f'ParRep::cube() not implemented yet.')
+        assert mx_dis > 0., f'ParRep::cube(): empty cube.'
+        print(f'Largest cube is defined by solutions ({s1}, {s2}), cube size = {mx_dis}.')
+        self.cubes.append(Cube(s1, s2, mx_dis))
+        asp = self.cubes[len(self.cubes) - 1].asp_vals
+        res = self.cubes[len(self.cubes) - 1].res_vals
+        return asp, res
+        # raise Exception(f'ParRep::cube() not implemented yet.')
