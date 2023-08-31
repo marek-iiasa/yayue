887b6c8e6e3e6ead4dadf55e761d3b5faa8d9315
Marek Makowski <marek@iiasa.ac.at>
2023-08-31 15:07:17 +0200

    mcma: ParRep exploration of plots
    Testing examples of diverse plots included. To be adapted for the ParRep class

--- models/mcma/driver.py
+++ models/mcma/driver.py
@@ -47,7 +47,7 @@ def driver(m1, ana_dir):    # m1 (core model) defined in main (mcma.py)
     # todo: consider log (complementary to *csv); open .../log.txt either for 'w' or 'a'
     # todo: implement rounding of floats (in printouts only or of all/most computed values?)
     n_iter = 1
-    max_itr = 20
+    max_itr = 10
     while n_iter <= max_itr:   # just for safety; should not be needed now
         i_stage = mc.set_stage()  # define/check current analysis stage
         print(f'\nAnalysis stage: {i_stage}, start iteration {n_iter}  -----------------------------------------------')
--- models/mcma/par_repr.py
+++ models/mcma/par_repr.py
@@ -189,4 +189,193 @@ class ParRep:     # representation of Pareto set
         print('\nList of cubes:')
         self.lst_cubes()
         print('\n')
+
+        self.plot2D()    # 2D plot
+
+        # todo: 3D plots need reconfiguration: either the default browser or Safari version: either Safari beta
+        # todo: or safari technology preview (see the Notes)
+        # self.plot3()
+        # self.plot3a()
         raise Exception(f'ParRep::summary() not implemented yet.')
+
+    def plot2D(self):
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        import mplcursors
+
+        # Create a sample DataFrame with data
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10],
+                'label': ['A', 'B', 'C', 'D', 'E']}  # Additional label column
+        df = pd.DataFrame(data)
+
+        # Create a scatter plot using Matplotlib
+        fig, ax = plt.subplots()
+        # scatter = ax.scatter(df['x'], df['y'], label='Data Points')
+        ax.scatter(df['x'], df['y'], label='Data Points')
+
+        # Add labels and title
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_title('Interactive Scatter Plot')
+
+        # Use mplcursors for interactive labels
+        cursor = mplcursors.cursor(hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(
+            f"Label: {df['label'][sel.index]}\nCoordinates: ({sel.target[0]:.2f}, {sel.target[1]:.2f})"))
+
+        plt.legend()
+
+        # Show the plot in a pop-up window
+        plt.show()
+
+    '''
+    # examples/temples below
+    def plot2D_ex(self):
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        import mplcursors
+
+        # Create a sample DataFrame with data
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10],
+                'label': ['A', 'B', 'C', 'D', 'E']}  # Additional label column
+        df = pd.DataFrame(data)
+
+        # Create a scatter plot using Matplotlib
+        fig, ax = plt.subplots()
+        # scatter = ax.scatter(df['x'], df['y'], label='Data Points')
+        ax.scatter(df['x'], df['y'], label='Data Points')
+
+        # Add labels and title
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_title('Interactive Scatter Plot')
+
+        # Use mplcursors for interactive labels
+        cursor = mplcursors.cursor(hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(
+            f"Label: {df['label'][sel.index]}\nCoordinates: ({sel.target[0]:.2f}, {sel.target[1]:.2f})"))
+        # cursor.connect("add", lambda sel: sel.annotation.set_text(
+        #     f"Label: {df['label'][sel.target.index]}\nCoordinates: ({sel.target[0]:.2f}, {sel.target[1]:.2f})"))
+
+        plt.legend()
+
+        # Show the plot in a pop-up window
+        plt.show()
+
+    def plot2Db(self):  # displays in the browser
+        import pandas as pd
+        import plotly.graph_objects as go
+
+        # Create a sample DataFrame with data
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10],
+                'label': ['A', 'B', 'C', 'D', 'E']}  # Additional label column
+        df = pd.DataFrame(data)
+
+        # Create a scatter plot using Plotly Graph Objects
+        fig = go.Figure()
+
+        # Add scatter trace with customized hover information
+        # scatter = fig.add_trace(go.Scatter(   # scatter object not needed here (but "scatter = " might be useful...)
+        fig.add_trace(go.Scatter(
+            x=df['x'],
+            y=df['y'],
+            mode='markers',
+            text=df['label'],  # The text to display in hover tooltip
+            hoverinfo='text+x+y',  # Display 'text', 'x', and 'y' in tooltip
+            marker=dict(size=10)
+        ))
+
+        # Update layout for better formatting
+        fig.update_layout(
+            title='Interactive Scatter Plot',
+            xaxis_title='X-axis',
+            yaxis_title='Y-axis'
+        )
+
+        # Show the plot
+        fig.show()
+    '''
+
+    '''
+    # version without hints extended by the content of another df column
+    # plt displays in a pop-up window, 
+    def plot2Da(self):
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        import mplcursors
+
+        # Create a sample DataFrame
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10]}
+        df = pd.DataFrame(data)
+
+        # Create a scatter plot
+        plt.scatter(df['x'], df['y'], label='Data Points')
+
+        # Add labels and title
+        plt.xlabel('X-axis')
+        plt.ylabel('Y-axis')
+        plt.title('Interactive Scatter Plot')
+
+        # Use mplcursors for interactive labels
+        cursor = mplcursors.cursor(hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(f"({sel.target[0]:.2f}, {sel.target[1]:.2f})"))
+
+        plt.legend()
+        plt.show()
+    '''
+
+
+'''
+# just templates/examples, to be adapted (also WebGL problem [see the TODO above] has to be fixed)
+    def plot3(self):
+        import pandas as pd
+        import matplotlib.pyplot as plt
+        import mplcursors
+
+        # Create a sample DataFrame with 3D data
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10],
+                'z': [10, 8, 6, 4, 2]}
+        df = pd.DataFrame(data)
+
+        # Create a 3D scatter plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        scatter = ax.scatter(df['x'], df['y'], df['z'], label='Data Points')
+
+        # Add labels and title
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('Z-axis')
+        ax.set_title('3D Scatter Plot')
+
+        # Use mplcursors for interactive labels
+        cursor = mplcursors.cursor(hover=True)
+        cursor.connect("add", lambda sel: sel.annotation.set_text(
+            f"({sel.target[0]:.2f}, {sel.target[1]:.2f}, {sel.target[2]:.2f})"))
+
+        plt.legend()
+        plt.show()
+
+    def plot3a(self):
+        # Create a sample DataFrame with 3D data
+        import pandas as pd
+        import plotly.express as px
+
+        # Create a sample DataFrame with 3D data
+        data = {'x': [1, 2, 3, 4, 5],
+                'y': [2, 4, 6, 8, 10],
+                'z': [10, 8, 6, 4, 2]}
+        df = pd.DataFrame(data)
+
+        # Create an interactive 3D scatter plot using Plotly
+        fig = px.scatter_3d(df, x='x', y='y', z='z', title='3D Scatter Plot',
+                            labels={'x': 'X-axis', 'y': 'Y-axis', 'z': 'Z-axis'})
+
+        # Show the plot
+        fig.show()
+'''
