b52195102ef0bcc41b017a73bd8f95974ce1f451
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2023-11-30 00:21:31 +0800

    create inst for mk_model

---
+++ models/matouqin/inst.py
@@ -0,0 +1,16 @@
+import pyomo.environ as pe       # more robust than using import *
+
+
+# return the model instance (currently the params are in *.dat, i.e., AMPL-like format
+def mk_model(m, v1):    # m: abstract/symbolic model, f_data: parameters in AMPL-like format
+    # data = DataPortal()  # the default arg: (model=model) !
+    # data.load(filename='data0.yaml')  # works with DataPortal() and DataPortal(model=m)
+    # data.load(filename='data0.json')  # works with DataPortal() and DataPortal(model=m)
+    data = pe.DataPortal(model=m)  # parameter (model=m) needed for loading *.dat
+    # dat-format requires DataPortal(model=m)
+    data.load(filename=v1)  # v1 prepared by Zixuan
+    # mod = m.create_instance(data)
+
+    # print('\n instance.pprint() follows      -----------------------------------------------------------------')
+    # mod.pprint()
+    # print('end of instance printout          -----------------------------------------------------------------\n')
