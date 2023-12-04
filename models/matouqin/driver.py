e4e1627b408282d9e14e0744a28c45fb769d3faa
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2023-12-05 07:04:30 +0800

    add print

--- models/matouqin/driver.py
+++ models/matouqin/driver.py
@@ -6,11 +6,11 @@ Prototype of the Matouqin driver
 import sys		# needed for sys.exit()
 import os
 # import pandas as pd
-import pyomo.environ as pe
-import inst
+# import pyomo.environ as pe
 from pyomo.opt import SolverStatus
 from pyomo.opt import TerminationCondition
-from sms import Model  # handles sub model/block of AF and links to the core/substantive model
+from inst import *
+from sms import *  # handles sub model/block of AF and links to the core/substantive model
 # from params import Params
 from report import report
 
@@ -37,25 +37,25 @@ def driver():
     data_dir = f'{path}/Data/'
 
     # make model
-    abst = Model(False)    # initialize Model class that generates model instance (ConcreteModel)
+    abst = mk_sms()    # initialize Model class that generates model instance (ConcreteModel)
     # par = Params()  # prepare all model parameters
-    data_v1 = f'{data_dir}dat1.dat'
-    m = inst.mk_model(abst, data_v1)
-    print(f'\nAnalysing instance of model {m.name}.')
+    f_data = f'{data_dir}dat1.dat'
+    model = inst(abst, f_data)
+    print(f'\nAnalysing instance of model {model.name}.')
 
-    m.pprint()
+    model.pprint()
 
     print('\nsolving --------------------------------')
     # select solver
     opt = pe.SolverFactory('glpk')
     # opt = pe.SolverFactory('ipopt') # solves both LP and NLP
     # opt = SolverFactory('gams')  # gams can be used as a solver
-    results = opt.solve(m, tee=False)   # True to pipe output to the terminal
+    results = opt.solve(model, tee=False)   # True to pipe output to the terminal
     chk_sol(results)  # check the status of the solution
     # todo: clarify exception (uncomment next line) while loading the results
     # m.load(results)  # Loading solution into results object
 
     print('\nprocessing solution --------------------------------')
-    report(m)
+    report(model)
 
     # opt = SolverFactory('gams')  # gams can be used as a solver
