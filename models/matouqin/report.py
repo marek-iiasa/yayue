0f77101176bd85258c54ce0b9864b2333feef4a4
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2024-01-05 02:50:40 +0800

    readjust, copy from pipa 'report'

--- models/matouqin/report.py
+++ models/matouqin/report.py
@@ -1,33 +1,60 @@
 """
-Report writer
+Reporting results
 """
 # import os.path
 
 # import sys		# needed for sys.exit()
 # import os
-# import pandas as pd
+import pandas as pd
 import pyomo.environ as pe  # needed for extracting elements of the solution
 
 
-def report(m):
 
-    # print(f'\nPlace holder for report results of model {m.name}.')
-    # revenue = f'{pe.value(model.revenue):.3e}'      # scientific counting method, accurate to three decimal places
+class Report:
+    # cf regret::report() for extensive processing
+    def __init__(self, m1, rep_dir, rep_vars):
+        self.m1 = m1    # core-model (used for extracting from solutions values of variables)
+        self.rep_dir = rep_dir  # repository for storing results
+        self.rep_vars = rep_vars    # names of the core-model variables to be included in the report
+        self.sol_vars = []  # rows with values of vars in self.sol_vars, each row for one solution/iteration
+        self.df_vars = None     # df with values (for each iter) of the vars defined in self.sol_vars
+        self.f_df_vars = f'{rep_dir}/df_vars.csv'  # file name of the stored df
 
-    print('\nValues of inflow ----------------------------------------------------------------------------')
-    # print(f'Total storage capacity = {pe.value(m.inflow)}MW')
-    ave_inflow = sum(m.inflow[t] for t in m.T) / m.nHrs
-    print(f'Average inflow = {ave_inflow} MW')
+        print(f'\nReport ctor: handling results of the Matouqin model.     -------------')
 
-    print('\nValues of decision variables ----------------------------------------------------------------------------')
-    print(f'Supply = {pe.value(m.supply)} MW')
-    # print(f'Numbers of storage devices = {pe.value(m.sNum)}')
+    # extract and store values of the variables to be included in the report
+    # parse indexed and un-indexed variable, rewrite the values
+    def var_vals(self):
+        vals = []   # tmp list of lists, each for one var: [var_name, is_indexed, val(s)]
+        m1_vars = self.m1.component_map(ctype=pe.Var)  # all variables of the m1 (core model)
+        for var_name in self.rep_vars:     # loop over m1.vars of all requested var-names
+            m1_var = m1_vars[var_name]
+            if m1_var is None:
+                raise Exception(f'Variable {var_name} is not defined in the core model.')
+            if m1_var.is_indexed():
+                val_dict = m1_var.extract_values()  # values returned in dict (indexes as keys)
+                vals.append([var_name, True, val_dict])     # True: if m1_var is is indexed
+                # print(f'Values of indexed variable {var_name} = {val_dict}')
+            else:
+                val = m1_var.value
+                vals.append([var_name, False, val])
+                # print(f'Value of the report variable {var_name} = {val}')
 
-    print('\nValues of outcome variables -----------------------------------------------------------------------------')
-    print(f'Total revenue  = {pe.value(m.revenue)} million RMB')
-    print(f'Income  = {pe.value(m.income)} million RMB')
-    print(f'Investment cost  = {pe.value(m.invCost)} million RMB')
-    print(f'Operation and maintenance cost  = {pe.value(m.OMC)} million RMB')
-    print(f'Surplus cost  = {pe.value(m.overCost)} million RMB')
-    print(f'Shortage cost  = {pe.value(m.buyCost)} million RMB')
-    print(f'Balance cost  = {pe.value(m.balCost)} million RMB')
+        # parse the extracted values into entries of a row to be included by the self.summary() in the df_vars
+        new_row = {}
+        for item in vals:
+            if item[1] is False:    # simple (not indexed) var
+                new_row.update({item[0]: f'{item[2]:.2e}'})
+            else:       # indexed var
+                d_item = item[2]    # dict with values of indexed var
+                for (ind, val) in d_item.items():       # for example: sCap_cell1 : 50
+                    idx = f'{item[0]}_{ind}'
+                    new_row.update({idx: f'{val:.2e}'})
+        self.sol_vars.append(new_row)   # append to the list of rows
+
+    # generate and store df's with info on criteria and the variables requested for report/plots
+    def summary(self):
+        self.df_vars = pd.DataFrame(self.sol_vars)
+        self.df_vars.to_csv(self.f_df_vars, index=True)
+        print(f'\nValues of core-model variables requested to be reported are stored in the DataFrane '
+              f'"{self.f_df_vars}" file.')
