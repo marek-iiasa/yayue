9480d09938ae42c9992ddeba2bc29585000122d8
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2024-01-05 02:49:33 +0800

    check and correct

--- models/matouqin/sms.py
+++ models/matouqin/sms.py
@@ -17,17 +17,15 @@ def mk_sms():      # p: model parameters prepared in the Params class
 
     # sets
     # noinspection SpellCheckingInspection
-    m.Se = pe.Set()
-    m.Sh = pe.Set()
-    m.Sc = pe.Set()
+    m.Se = pe.Set()     # set of electrolizers
+    m.Sh = pe.Set()     # set of hydrogen tanks
+    m.Sc = pe.Set()     # set of fuel cells
 
     # noinspection ClassSet. The union function works.
-    m.S = pe.Set(initialize=m.Se | m.Sh | m.Sc)
-    # m.nHrs = pe.Param()
-    # m.nHrs_ = pe.Param()
-    m.nHrs = pe.Param(domain=pe.PositiveIntegers)       # the number of hours (time periods) in a year
+    m.S = pe.Set(initialize=m.Se | m.Sh | m.Sc)     # set of storage devices
 
-    # noinspection typeInspection on line31
+    # noinspection typeInspection
+    m.nHrs = pe.Param(domain=pe.PositiveIntegers)       # the number of hours (time periods) in a year
     m.nHrs_ = pe.Param(initialize=m.nHrs - 1)     # index of the last time periods (hour)
     m.T = pe.RangeSet(0, m.nHrs_)       # set of time periods (hour)
 
@@ -88,32 +86,6 @@ def mk_sms():      # p: model parameters prepared in the Params class
     m.sInv = pe.Param(m.S, domain=pe.NonNegativeReals)      # per unit investment cost of storage, [million RMB]
     m.sOmc = pe.Param(m.S, domain=pe.NonNegativeReals)       # per unit operation cost of storage, [million RMB]
 
-    # Relations defining auxiliary variables
-    @m.Constraint(m.S)     # Total capacity of s-th type of storage device
-    def sCapC(mx, s):
-        return mx.sCap[s] == mx.mxCap[s] * mx.sNum[s]
-
-    @m.Constraint(m.Sh)      # Lowest amount of hydrogen needed in s-th of tank
-    def hMinC(mx, s):
-        return mx.hMin[s] == mx.hmi[s] * mx.sNum[s]
-
-    @m.Constraint(m.Sh)     # Maximum hydrogen inflow of s-th tank (per hour)
-    def mxInC(mx, s):
-        return mx.mxIn[s] == mx.hMxIn[s] * mx.sNum[s]
-
-    @m.Constraint(m.Sh)
-    def mxOutC(mx, s):
-        return mx.mxOut[s] == mx.hMxOut[s] * mx.sNum[s]
-
-    @m.Constraint(m.T)
-    def eSurplusC(mx, t):
-        return mx.eSurplus[t] == mx.inflow[t] - mx.dOut[t] - mx.sIn[t] - mx.ePrs[t]
-
-    @m.Constraint(m.T)
-    def eBoughtC(mx, t):
-        return mx.eBought[t] == mx.supply - mx.dOut[t] - mx.sOut[t]
-
-    # relations defining energy flows
     @m.Constraint(m.T)      # energy inflow is divided into four energy flows
     def inflow_blc(mx, t):
         return mx.inflow[t] == mx.dOut[t] + mx.sIn[t] + mx.ePrs[t] + mx.eSurplus[t]
@@ -137,39 +109,44 @@ def mk_sms():      # p: model parameters prepared in the Params class
     @m.Constraint(m.T)      # The amount of electricity used for pressure control of all hydrogen tanks
     def ePrs_blc(mx, t):
         return mx.ePrs[t] == sum(mx.eph2[s] * mx.hIn[s, t] for s in mx.Sh)
+    # Relations defining auxiliary variables
 
-    @m.Constraint(m.Sh, m.T)        # Amount of hydrogen in each tank type
+    @m.Constraint(m.Sh, m.T)  # Amount of hydrogen in each tank type
     def hVol_bal(mx, s, t):
         if t == 0:
             return mx.hVol[s, t] == mx.hMin[s]
         else:
-            return mx.hVol[s, t] == mx.h2Res[s] * mx.hVol[s, (t-1)] + mx.hIn[s, t] - mx.hOut[s, t]
+            return mx.hVol[s, t] == mx.h2Res[s] * mx.hVol[s, (t - 1)] + mx.hIn[s, t] - mx.hOut[s, t]
 
-    @m.Constraint(m.Sh, m.T)        # Amount of hydrogen in tanks of each type
+    @m.Constraint(m.Sh, m.T)  # Amount of hydrogen in tanks of each type
     def hVol_lower(mx, s, t):
         return mx.hVol[s, t] >= mx.hMin[s]
 
-    @m.Constraint(m.Sh, m.T)        # Amount of hydrogen in tanks of each type
+    @m.Constraint(m.Sh, m.T)  # Amount of hydrogen in tanks of each type
     def hVol_upper(mx, s, t):
         return mx.hVol[s, t] <= mx.sCap[s]
 
-    @m.Constraint(m.Sh, m.T)        # Maximum flow to each type of tank
-    def hIn_upper(mx, s, t):
-        return mx.hIn[s, t] <= mx.hMxIn[s]
+    @m.Constraint(m.Sh, m.T)  # Maximum flow to each type of tank must lower than the available space
+    def hIn_upper1(mx, s, t):
+        return mx.hIn[s, t] <= mx.sCap[s] - mx.hVol[s, t]
 
     @m.Constraint(m.Sh, m.T)        # Maximum flow from each type of tank to fuel cells
-    def hOut_upper(mx, s, t):
-        return mx.hOut[s, t] <= mx.hMxOut[s]
+    def hIn_upper2(mx, s, t):
+        return mx.hIn[s, t] <= mx.hMxIn[s]
 
-    # @m.Constraint(m.Sh, m.T)      # Constraint on one tank cannot support inflow and outflow at the same time
-    # def htankC(mx, s, t):
-    #    return mx.hIn[s, t] * mx.hOut[s, t] == 0
+    @m.Constraint(m.Sh, m.T)        # Maximum flow from each type of tank
+    def hOut_upper1(mx, s, t):
+        return mx.hOut[s, t] <= mx.hVol[s, t] - mx.hMin[s]
+
+    @m.Constraint(m.Sh, m.T)        # Maximum flow from each type of tank to fuel cells
+    def hOut_upper2(mx, s, t):
+        return mx.hOut[s, t] <= mx.hMxOut[s]
 
     @m.Constraint(m.T)      # Hydrogen flow from tanks to all fuel cells
     def h2C_blc1(mx, t):
         return mx.h2C[t] == sum(mx.hOut[s, t] for s in mx.Sh)
 
-    @m.Constraint(m.T)      # Hydrogen flow from tanks is split between fuel cells
+    @m.Constraint(m.T)  # Hydrogen flow from tanks is split between fuel cells
     def h2C_blc2(mx, t):
         return mx.h2C[t] == sum(mx.hInc[s, t] for s in mx.Sc)
 
@@ -217,6 +194,23 @@ def mk_sms():      # p: model parameters prepared in the Params class
     @m.Constraint()     # Total revenue of the system
     def revenueC(mx):
         return mx.revenue == mx.income - mx.invCost - mx.OMC - mx.balCost
+
+    # Relations defining auxiliary variables
+    @m.Constraint(m.S)
+    def sCapC(mx, s):
+        return mx.sCap[s] == mx.mxCap[s] * mx.sNum[s]
+
+    @m.Constraint(m.Sh)
+    def hMinC(mx, s):
+        return mx.hMin[s] == mx.hmi[s] * mx.sNum[s]
+
+    @m.Constraint(m.Sh)  # Maximum hydrogen inflow of s-th tank (per hour)
+    def mxInC(mx, s):
+        return mx.mxIn[s] == mx.hMxIn[s] * mx.sNum[s]
+
+    @m.Constraint(m.Sh)
+    def mxOutC(mx, s):
+        return mx.mxOut[s] == mx.hMxOut[s] * mx.sNum[s]
 
     @m.Objective(sense=pe.maximize)     # Revenue (used as a Goal-Function/Objective in single-criterion analysis)
     def obj(mx):
