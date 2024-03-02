aba37c1551dcc86b50ac438d8f190c91d6e10d04
Marek Makowski <marek@iiasa.ac.at>
2024-03-02 12:38:34 +0100

    mcma: wdir reorganization and update
    All Template/test files for distribution are from now in the wdir.

--- models/mcma/Templates/example.py
+++
@@ -1,7 +0,0 @@
-""" Place holder for the demo-model with export function (to be prepared by JG)."""
-#
-# very simple model, with hard-coded name, generating *.dll in the current dir
-# e.g., example.dll
-
-# there should be enough variables to define at least two criteria, preferably
-# one min and one max
--- models/mcma/wdir/Templates/Readme.txt
+++
@@ -1,3 +0,0 @@
-Here the templates are provided:
-- cfg.yml - template of analysis configuration (all options commented)
-- model.py - trivial model developed in Pyomo, includes the export in the dll format
