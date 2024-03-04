22531a379ccf9ecf0a3c675d24922d2633297b18
Marek Makowski <marek@iiasa.ac.at>
2024-03-04 22:57:31 +0100

    mcma: main() improved
    Flow-control enhanced for a better detection of the run situation (as the module in development-env. or as included in pymcma). Also minos improvements of the code.

--- models/mcma/mcma.py
+++ models/mcma/mcma.py
@@ -2,10 +2,8 @@
 
 """ Main function of the mcma: import the core model, define the work-space. """
 
-# todo: check consistency of using scaled and not-scaled entities
 # todo: add/print info:
 #   A/R undefined for Pareto set corners (virtual solutions, i.e., no values of model variables)
-#   size of cubes in scaled coordinates, values of A/R in native (model-vars) scales
 
 # from os import R_OK, access
 # from os.path import isfile
@@ -74,32 +72,29 @@ def main():
     tstart = dt.now()
     # print('Started at:', str(tstart))
 
-    # wdir = '.'     # current dir is the wdir for both development and packaged version
-    # assert os.path.exists(wdir), f'The work directory "{wdir}" does not exist'
-    # os.chdir(wdir)
-    # process cmd-line args (currently only either install or ana_dir)
     module_dir = 'mcma'
-    as_module = os.path.exists(module_dir)  # true, if run as module
+    module_py = 'driver.py'
+    as_module = os.path.exists(f'{module_dir}/{module_py}')  # true, if run as module, false if as pymcma
     if as_module:
         os.chdir(module_dir)
-        print(f'Current working directory changed to ./{module_dir}')
+        print(f'Current working directory changed to "./{module_dir}".')
     args = read_args()
     # ana_dir = args.ana_id or 'anaTst'
     install = args.install
     ana_dir = args.anaDir
-    if install:
+    if install:     # check, if wdir (composed of pymcma's templates and tests) should be installed
         assert ana_dir is None, f'ERROR: no directory should be defined for the installation.'
         ana_dir = 'anaTst'
         if as_module:
             print('Skip installation of test directories in the development mode.')
-        else:
+        else:       # run as distributed pymcma
             print('Installing test directories.')
             create_wdir()
     else:
         assert ana_dir is not None, f'ERROR: analysis directory should be defined.'
     print(f'Analysis directory: {ana_dir}')
     assert os.path.exists(ana_dir), f'The analysis directory "{ana_dir}" does not exist'
-    os.chdir(ana_dir)
+    os.chdir(ana_dir)   # analysis run in the choosen/dedicated directory
 
     # process the run configuration options and configure the working space
     config = Config()    # process yaml config. file
@@ -122,7 +117,7 @@ def main():
         f_out = open(fn_out, 'w')
         sys.stdout = f_out
 
-    driver(cfg)  # driver and all classes get all needed info from the cfg dict
+    driver(cfg)  # driver and all needed objects of classes get all needed params from the cfg dict
 
     tend = dt.now()
     print('\nStarted at: ', str(tstart))
