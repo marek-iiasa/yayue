0fcfdb991dc1e236655cbb266186e8dcb330029a
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2024-03-22 01:17:00 +0800

    Matouqin: Rename file and recheck formulations of parameter generation

--- models/matouqin/dat_process.py
+++ models/matouqin/dat_process.py
@@ -8,37 +8,45 @@ import os
 
 
 class Params:
-    def __init__(self, data_excel):
+    def __init__(self, data_dir, data_excel, name_ampl):
+        self.dat_dir = data_dir
+        self.f_dat = data_excel
+        self.name_ampl = name_ampl
 
-        self.cf_df = pd.read_excel(data_excel, sheet_name='cf')
-        self.gen_df = pd.read_excel(data_excel, sheet_name='generation')
-        self.time_df = pd.read_excel(data_excel, sheet_name='time')
-        self.price_df = pd.read_excel(data_excel, sheet_name='price')
-        self.elec_df = pd.read_excel(data_excel, sheet_name='electrozer')
-        self.hytank_df = pd.read_excel(data_excel, sheet_name='tank')
-        self.fcell_df = pd.read_excel(data_excel, sheet_name='fuel-cell')
-
-        # define unit conversion factors
-        self.cH2 = 0.0899   # conversion factor between hydrogen gas volume and weight, [kg/m3]
-        self.cEle = 1000    # conversion factor between MW and kW
-        self.hCal = 1.42351e5  # calorific value of hydrogen, [kJ/kg]
-        self.eCal = 3.6e3  # calorific value of electricity, [kJ/kWh]
+        # read initial data from excel file
+        self.cf_df = pd.read_excel(self.f_dat, sheet_name='cf', nrows=350)
+        self.gen_df = pd.read_excel(self.f_dat, sheet_name='generation')
+        self.time_df = pd.read_excel(self.f_dat, sheet_name='time')
+        self.price_df = pd.read_excel(self.f_dat, sheet_name='price')
+        self.elec_df = pd.read_excel(self.f_dat, sheet_name='electrozer')
+        self.hytank_df = pd.read_excel(self.f_dat, sheet_name='tank')
+        self.fcell_df = pd.read_excel(self.f_dat, sheet_name='fuel-cell')
 
         # print data read from excel file
-        print(f'Capacity factor:\n {self.cf_df.head()}')
-        print(f'Generation: \n {self.gen_df}')
-        print(f'Time related parameters:\n {self.time_df}')
-        print(f'Price related parameters:\n {self.price_df}')
-        print(f'Electrolyzers:\n {self.elec_df}')
-        print(f'Hydrogen tanks:\n {self.hytank_df}')
-        print(f'Fuel-cells: \n {self.fcell_df}')
+        # print(f'Capacity factor:\n {self.cf_df.head()}')
+        # print(f'Generation: \n {self.gen_df}')
+        # print(f'Time related parameters:\n {self.time_df}')
+        # print(f'Price related parameters:\n {self.price_df}')
+        # print(f'Electrolyzers:\n {self.elec_df}')
+        # print(f'Hydrogen tanks:\n {self.hytank_df}')
+        # print(f'Fuel-cells: \n {self.fcell_df}')
 
+        # define default unit conversion factors
+        self.cH2 = 0.0899   # conversion factor between hydrogen gas volume and weight, [kg/m3]
+        self.cEle = 1000    # conversion factor between MW and kW, 1 MW = 1000 KW
+        self.hCal = 33.3  # calorific value of hydrogen, [kWh/kg]
+        # self.hCal = 1.42351e5  # calorific value of hydrogen, [kJ/kg]
+        # self.eCal = 3.6e3  # calorific value of electricity, [kJ/kWh]
+
+        # define default parameters
         self.inflow = None
         self.Hrs = self.nHrs = self.ydis = None
         self.penalty = self.ePrice = self.eOver = self.eBprice = None
         self.eh2 = self.eph2 = self.h2Ratio = self.h2Res = self.h2e = None
         self.sInv = None
 
+        # define default parameters generation
+        print(f'Data processing begin:')
         self.set_inflow()
         self.set_time()
         self.set_price()
@@ -46,10 +54,13 @@ class Params:
         self.set_hyTank()
         self.set_fcell()
 
-        self.write_to_file()
-        self.write_to_excel()
+        # store parameters
+        # self.write_to_ampl()
+        # self.write_to_excel()
 
     # data processing
+
+    # 1) generated inflows
     def set_inflow(self):    # generation
         dv_inflow = pd.DataFrame()
         for index, row in self.gen_df.iterrows():
@@ -59,14 +70,15 @@ class Params:
 
             # check generation devices name is consistent
             if name in self.cf_df.columns:
-                dv_inflow[f'inflow_{name}'] = self.cf_df[name] * capacity * number
+                dv_inflow[f'inflow_{name}'] = self.cEle * self.cf_df[name] * capacity * number
             else:
                 print(f"Warning: '{name}' not found in {self.cf_df}")
 
         self.inflow = dv_inflow.sum(axis=1)
 
-        print(f'Inflow:\n {self.inflow.head()}')
+        print(f'Inflow:\n {self.inflow.head(12)}')
 
+    # 2) set time related parameters
     def set_time(self):
         self.Hrs = self.time_df.loc[0, 'Hrs']       # the duration time between each decision period (hour)
         self.nHrs = len(self.cf_df)     # the number of hours in a year
@@ -76,6 +88,7 @@ class Params:
 
         print(f'Time related parameters are added')
 
+    # 3) set price unit in MW/h
     def set_price(self):
         ep = self.price_df.loc[0, 'eP']  # unit contract price, unit in [RMB/kWh]
         oc = self.price_df.loc[0, 'eO']  # unit price of managed electricity surplus, unit in [RMB/kWh]
@@ -91,6 +104,7 @@ class Params:
 
         self.add_price()
 
+    # 4) generate electricity prices (purchased/loss)
     def add_price(self):
         self.eBprice = self.penalty * self.ePrice  # purchase price of buying electricity, unit in [million RMB/MWh]
 
@@ -99,10 +113,15 @@ class Params:
         print(f'Price related parameters are updated')
         print(f'New price information:\n {self.price_df}')
 
+    # 5) read and generate new parameters related to electrolyzers
     def set_elec(self):
         eh2 = 1 / self.elec_df['toHprd'] * self.cEle * self.cH2
+        # annuity factor used to generate annualized investment cost (pay at the end of year)
         invshare = (((1 + self.ydis) ** self.elec_df['n'] * self.ydis)
                     / ((1 + self.ydis) ** self.elec_df['n'] - 1))
+        # annuity factor used to generate annualized investment cost (pay at the beginning of year)
+        # invshare = (((1 + self.ydis) ** (self.elec_df['n']+1) * self.ydis)
+        #             / ((1 + self.ydis) ** self.elec_df['n'] - 1))
         sinv = invshare * self.elec_df['sInvcost']
 
         self.elec_df['eh2'] = eh2
@@ -112,11 +131,16 @@ class Params:
         print(f'New parameters of electrolyzers are added')
         print(f'{self.elec_df}')
 
+    # 6) read and generate new parameters related to hydrogen tanks
     def set_hyTank(self):
         eph2 = (1 / self.cEle) * self.hytank_df['toHstr']
         h2res = 1 - self.hytank_df['h2Loss']
+        # annuity factor used to generate annualized investment cost (pay at the end of year)
         invshare = (((1 + self.ydis) ** self.hytank_df['n'] * self.ydis)
                     / ((1 + self.ydis) ** self.hytank_df['n'] - 1))
+        # annuity factor used to generate annualized investment cost (pay at the beginning of year)
+        # invshare = (((1 + self.ydis) ** (self.hytank_df['n'] + 1) * self.ydis)
+        #             / ((1 + self.ydis) ** self.hytank_df['n'] - 1))
         sinv = invshare * self.hytank_df['sInvcost']
 
         self.hytank_df['eph2'] = eph2
@@ -127,11 +151,16 @@ class Params:
         print(f'New parameters of hydrogen tanks are added')
         print(f'{self.hytank_df}')
 
+    # 7) read and generate new parameters related to fuel-cells
     def set_fcell(self):
-        h2ratio = (self.hCal / self.eCal) * (1 / self.cEle)
+        h2ratio = self.hCal * (1 / self.cEle)
         h2e = h2ratio * self.fcell_df['e']
+        # annuity factor used to generate annualized investment cost (pay at the end of year)
         invshare = (((1 + self.ydis) ** self.fcell_df['n'] * self.ydis)
                     / ((1 + self.ydis) ** self.fcell_df['n'] - 1))
+        # annuity factor used to generate annualized investment cost (pay at the beginning of year)
+        # invshare = (((1 + self.ydis) ** (self.fcell_df['n'] + 1) * self.ydis)
+        #             / ((1 + self.ydis) ** self.fcell_df['n'] - 1))
         sinv = invshare * self.fcell_df['sInvcost']
 
         self.fcell_df['h2ratio'] = h2ratio
@@ -142,6 +171,7 @@ class Params:
         print(f'New parameters of fuel-cells are added')
         print(f'{self.fcell_df}')
 
+    # 8) write parameters used in matouqin model to ampl format
     def to_ampl(self):
         nhrs_str = '\nparam nHrs :=\n'
 
@@ -238,16 +268,18 @@ class Params:
 
         return ampl_dat
 
-    def write_to_file(self):
+    # 9) write ample format parameters to ampl file
+    def write_to_ampl(self):
         ampl_dat = self.to_ampl()
-        filename = f'{data_dir}dat1.dat'
+        filename = f'{self.dat_dir}{self.name_ampl}.dat'
         with open(filename, 'w') as file:
             file.write(f'# test data for Matouqin \n')
             file.write(ampl_dat)
-        print(f"Data written to {filename}")
+        print(f"Data written to ampl file: {filename}")
 
+    # 10) store all parameters in excel file
     def write_to_excel(self):
-        excel_file = f'{data_dir}all_params.xlsx'
+        excel_file = f'{self.dat_dir}all_params.xlsx'
 
         with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
             self.cf_df.to_excel(writer, sheet_name='cf', index=False)
@@ -258,8 +290,12 @@ class Params:
             self.hytank_df.to_excel(writer, sheet_name='hytank', index=False)
             self.fcell_df.to_excel(writer, sheet_name='fuel-cell', index=False)
 
+        print(f'All parameters are stored in: {excel_file}')
+
 
 path = '.'
-data_dir = f'{path}/Data/'
-f_data = f'{data_dir}dat1.xlsx'
-par = Params(f_data)
+dat_dir = f'{path}/Data/'
+f_data = f'{dat_dir}dat1.xlsx'
+par = Params(dat_dir, f_data, 'dat1')   #
+par.write_to_ampl()
+par.write_to_excel()
