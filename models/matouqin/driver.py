04430691037a90a2b2d94bb821374923bbb8f952
Zixuan-Zhang <49399874+Zadie-Zhang@users.noreply.github.com>
2024-03-27 01:36:04 +0800

    Matouqin: add report results to excel file

--- models/matouqin/driver.py
+++ models/matouqin/driver.py
@@ -11,9 +11,8 @@ from pyomo.opt import SolverStatus
 from pyomo.opt import TerminationCondition
 from inst import *
 from sms import *  # handles sub model/block of AF and links to the core/substantive model
-# from params import Params
-from report import *
-from plot1 import *
+# rom dat_process import Params  # data processing
+from report import *    # report all variables and needed results for plotting
 
 
 def chk_sol(res):  # check status of the solution
@@ -34,7 +33,6 @@ def driver():
     path = '.'
     data_dir = f'{path}/Data/'      # repository of data
     res_dir = f'{path}/Results/'    # repository of results
-    fig_dir = f'{path}/Figures/'    # repository of figures
 
     # make model
     abst = mk_sms()    # initialize Model class that generates model instance (ConcreteModel)
@@ -49,7 +47,7 @@ def driver():
     print('\nsolving --------------------------------')
     # select solver
     opt = pe.SolverFactory('glpk')
-    # opt = pe.SolverFactory('ipopt') # solves both LP and NLP
+    # opt = pe.SolverFactory('ipopt')  # solves both LP and NLP
     # opt = SolverFactory('gams')  # gams can be used as a solver
     # opt = SolverFactory('gams')  # gams can be used as a solver
     results = opt.solve(model, tee=True)   # True to pipe output to the terminal
@@ -67,43 +65,18 @@ def driver():
     print(f'Values of the following variables will be extracted from the solution and stored in the df:\n{rep_vars}')
 
     rep = Report(model, res_dir, rep_vars)      # report results
-    rep.var_vals()  # extract from the solution values of the requessted variables
+    rep.var_vals()  # extract from the solution values of the requested variables
     rep.summary()  # store the extracted values in a df
-
-    # flow_vars = ['hVol']
-    # flow = Report(model, res_dir, flow_vars)
-    # flow.flow_report()
-    # flow.summary()
+    rep.toExcel()   # store the extracted values in excel for plotting
 
     print(f'\nPlace holder for report results of model {model.name}.')
-    # revenue = f'{pe.value(model.revenue):.3e}'      # scientific counting method, accurate to three decimal place
+
     print('\nValues of inflow ----------------------------------------------------------------------------')
-    # m.inflow.display()
     ave_inflow = sum(model.inflow[t] for t in model.T) / model.nHrs
     print(f'Average inflow = {ave_inflow} MW')
 
     print('\nValues of decision variables ------------------------------------------------------------------------')
     print(f'Supply = {pe.value(model.supply)} MW')
-    # for i in model.sNum_index:
-    #    print(f'sNum[{i}] = {pe.value(m.sNum[i])}')
-    # for i in model.sCap_index:
-    #    print(f'sCap[{i}] = {pe.value(m.sCap[i])}')
-    model.sNum.display()
-    model.sCap.display()
-    # print(f'Numbers of storage devices = {pe.value(m.sNum)}')
-
-    # print('\nEnergy flows ------------------------------------------------------------------------')
-    # model.supply.display()
-    # model.eSurplus.display()
-    # model.eBought.display()
-    # model.dOut.display()
-    # model.eIn.display()
-    # model.hIn.display()
-    # model.hOut.display()
-    # model.ePrs.display()
-    # model.mxIn.display()
-    # model.hVol.display()
-    # model.cOut.display()
 
     print('\nValues of outcome variables -------------------------------------------------------------------------')
     print(f'Total revenue  = {pe.value(model.revenue)} million RMB')
@@ -113,7 +86,3 @@ def driver():
     print(f'Surplus cost  = {pe.value(model.overCost)} million RMB')
     print(f'Shortage cost  = {pe.value(model.buyCost)} million RMB')
     print(f'Balance cost  = {pe.value(model.balCost)} million RMB')
-
-# ----------------------------------------------------------------
-# plotting
-    Plot(model, fig_dir)
